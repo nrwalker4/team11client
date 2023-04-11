64564d0b65fdd15c2dc24f7f036e96da86a441e7
Addie Ziegler <aziegler@crimson.ua.edu>
2023-04-11 17:01:34 -0500

    4.11

--- Resources/scripts/index.js
+++ Resources/scripts/index.js
@@ -1,2 +1,15 @@
-let smart = 0;
-let dumb = 1000;
+document.getElementById("gardenForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get the form data and do something with it
+  var gardenSize = document.getElementById("gardenSize").value;
+  var sunExposure = document.getElementById("sunExposure").value;
+  // Add more form field data as needed
+
+  // TODO: Process the form data and display recommended plant
+});
+
+// Clear modal form fields when modal is closed
+$('#exampleModal').on('hidden.bs.modal', function() {
+  $('#gardenForm').trigger("reset");
+});
