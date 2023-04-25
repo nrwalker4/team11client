e83e34010df21b63f80ada6aaaf7bb133090868c
Addie Ziegler <aziegler@crimson.ua.edu>
2023-04-25 10:26:01 -0500

    admin issues

--- Resources/scripts/admintest.js
+++ Resources/scripts/admintest.js
@@ -1,3 +1,22 @@
+function getPlants(plants) {
+    console.log(plants)
+    const plantsContainer = document.getElementById('plants-row'); // Replace with the ID of the container element on your webpage where you want to display the plants
+    plantsContainer.innerHTML = '';
+  
+    plants.forEach(plant => {
+      const plantElement = document.createElement('div');
+      plantElement.className = 'plant';
+      plantElement.innerHTML = `
+        <img src="${plant.imageLink}" alt="${plant.plantName}">
+        <h3>${plant.plantName}</h3>
+        <p>$${plant.price}</p>
+        <button onclick='addToCart(${plant})'>Add to Cart</button>
+      `;
+      plantsContainer.appendChild(plantElement);
+    });
+  }
+
+
 async function login() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -52,25 +71,6 @@ async function login() {
         //       // Handle error fetching image path
         //     }
           
-
-          function renderPlants(plants) {
-            console.log(plants)
-            const plantsContainer = document.getElementById('plants-row'); // Replace with the ID of the container element on your webpage where you want to display the plants
-            plantsContainer.innerHTML = '';
-          
-            plants.forEach(plant => {
-              const plantElement = document.createElement('div');
-              plantElement.className = 'plant';
-              plantElement.innerHTML = `
-                <img src="${plant.imageLink}" alt="${plant.plantName}">
-                <h3>${plant.plantName}</h3>
-                <p>$${plant.price}</p>
-                <button onclick='addToCart(${plant})'>Add to Cart</button>
-              `;
-              plantsContainer.appendChild(plantElement);
-            });
-          }
-  
           // Display the manager information on the page
           const managerTab = document.querySelector('.manager-tab');
           const managerInfoHTML = `
@@ -91,3 +91,49 @@ async function login() {
       console.error('Error logging in:', error);
     }
 }
+
+async function displayPlantEditForm(id) {
+    const songs = await fetch("https://localhost:5138/api/plant").then((res) =>
+      res.json()
+    );
+    const plant = plants.find((plant) => plant.plantID === id);
+    let html = `
+    <hr>
+    <h2>Edit Plant</h2>
+    <form id="editPlantForm">
+    <label for="name">Name:</label>
+    <input type="text" id="${id}-plantName" name="name" value="${plant.plantName}"/><br><br>
+    <label for="Type:">Type:</label>
+    <input type="text" id="${id}-plantType" name="type" value="${plant.plantType}"/><br><br>
+    <button type="button" onclick="handleEdit(${id})">Submit</button>
+  </form>`;
+    document.getElementById("editPlantForm").innerHTML = html;
+  }
+
+  async function handleEdit(id) {
+    const title = document.getElementById(`${id}-plantName`).value;
+    const artist = document.getElementById(`${id}-plantType`).value;
+    const songs = await fetch("https://localhost:5138/api/plant").then((res) =>
+      res.json()
+    );
+    const deleteSongApiUrl = "https://localhost:5138/api/plant";
+    const newPlant = plants.find((plant) => plant.plantID === id);
+    newPlant.plantName = plantName;
+    newPlant.plantType = plantType;
+    const response = await fetch('http://localhost:5138/api/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPlant)
+    })
+      .then(function (response) {
+        console.log(response);
+        getPlants(plants);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  
+    document.getElementById("editPlantForm").innerHTML = "";
+  }
